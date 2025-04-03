ce2376470cb7d63cb9ad9711675313d857028ff4
清清玄 <596484713@qq.com>
2025-04-03 14:44:17 +0800

    在 App.vue 中添加类型

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <n-notification-provider v-if="appCompoentUI === 'naive'">
+  <n-notification-provider v-if="appComponentUI === 'naive'">
     <n-message-provider>
       <router-view />
     </n-message-provider>
@@ -10,14 +10,17 @@
 <script lang="ts">
   import { defineComponent, provide, ref } from 'vue'
 
-  const appCompoentUI = ref((window as any).__APP_UI_COMPONENT__)
+  // 定义类型
+  type AppUIComponent = 'naive' | string;
+
+  const appComponentUI = ref<AppUIComponent>((window as any).__APP_UI_COMPONENT__);
 
   export default defineComponent({
     name: 'App',
     setup() {
-      provide('componentUIName', appCompoentUI)
+      provide('componentUIName', appComponentUI);
       return {
-        appCompoentUI,
+        appComponentUI,
       }
     },
   })
